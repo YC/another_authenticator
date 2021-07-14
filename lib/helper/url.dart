c47574bcd6f4a2caf371e459050ca71aba73b5e3
Steven Tang <steven@steventang.net>
2021-07-14 14:20:36 +1000

    fix: add launch external URL intent

--- lib/helper/url.dart
+++ lib/helper/url.dart
@@ -3,7 +3,7 @@ import 'package:url_launcher/url_launcher.dart' show canLaunch, launch;
 /// Launches a [url] inside the app.
 launchURL(String url) async {
   if (await canLaunch(url)) {
-    await launch(url, forceWebView: true);
+    await launch(url, forceWebView: false);
   } else {
     throw 'Could not launch $url';
   }
