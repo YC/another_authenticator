636cf56a6ac2400eed3d63efb18b558410c75a2f
Steven Tang <yc@steventang.net>
2024-09-28 23:19:19 +1000

    fix: import scope

--- otp/lib/otp.dart
+++ otp/lib/otp.dart
@@ -1,4 +1,6 @@
 library another_authenticator_otp;
 
 export 'models/otp_item.dart';
+export 'models/otp_type.dart';
+export 'models/otp_algorithm.dart';
 export 'otp/base32.dart';
--- state/test/legacy_repository_test.dart
+++ state/test/legacy_repository_test.dart
@@ -1,6 +1,5 @@
 import 'package:test/test.dart';
 
-import 'package:another_authenticator_otp/models/otp_type.dart';
 import 'package:another_authenticator_otp/otp.dart';
 import 'package:another_authenticator_state/legacy/legacy_repository.dart';
 import 'package:another_authenticator_state/state.dart';
