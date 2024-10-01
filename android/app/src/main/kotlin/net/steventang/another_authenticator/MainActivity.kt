9fe7194883890de0cf0b0589cf35dcf62b7077a7
Bhargav Raviya <46953122+bhargavraviya@users.noreply.github.com>
2024-10-01 16:13:16 +0530

    change android files (#15)
    * change android files
    * chnage comment from reviews
    * chore: fix trailing newlines
    * remove internet permissions
    * chore: add trailing newline
    
    ---------
    
    Co-authored-by: Steven Tang <yc@steventang.net>

--- android/app/src/main/kotlin/net/steventang/another_authenticator/MainActivity.kt
+++ android/app/src/main/kotlin/net/steventang/another_authenticator/MainActivity.kt
@@ -2,5 +2,4 @@ package net.steventang.another_authenticator
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
